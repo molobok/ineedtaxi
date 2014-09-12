d7ef8c51e167063edd306947b6734bbdbd4b1296
Jin Shun Chia <molobok@yahoo.com>
2014-09-12 11:03:52 +0800

    Initial Commit

---
+++ BetterTaxi7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BetterTaxi7
+//
+//  Created by Jin Shun Chia on 12/9/14.
+//  Copyright (c) 2014 theAppKitchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BetterTaxi7/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BetterTaxi7
+//
+//  Created by Jin Shun Chia on 12/9/14.
+//  Copyright (c) 2014 theAppKitchen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
